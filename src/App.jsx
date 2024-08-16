c961e441efdc6188ecd64ee7bd61bdada4ddf4c9
Jhon Alessandro <astralgmd@gmail.com>
2024-08-16 07:32:31 -0400

    feat(tracklist): add new tracklist layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
+
 import NavBar from "./components/NavBar/NavBar";
 import TrackList from "./components/TrackList/TrackList";
 import PlayList from "./components/PlayList/PlayList";
 
+import testData from "/public/test/data.json";
+
 function App() {
   /* Logic */
+  const [tracks, setTracks] = useState([]);
+  const [playlist, setPlaylist] = useState([...testData]);
+
+  function addTrack(newTrack) {
+    setTracks((prevTracks) => [...prevTracks, newTrack]);
+  }
 
   return (
     <>
       <NavBar />
       <div className="app__container">
-        <TrackList />
-        <PlayList />
+        <TrackList trackList={tracks} addTrack={addTrack} />
+        <PlayList playlistData={playlist} />
       </div>
     </>
   );
--- src/components/TrackList/TrackList.jsx
+++ src/components/TrackList/TrackList.jsx
@@ -1,7 +1,39 @@
-function TrackList() {
+import trackListImage from "../../../public/images/empty-tracklist-image.png";
+import "./TrackList.css";
+
+function TrackList({ trackList, addTrack }) {
   return (
     <>
-      <h1>TrackList</h1>
+      <div className="tracklist">
+        {(trackList.length === 0 && <EmptyTrackList />) || (
+          <TrackListRender tracksData={trackList} addTrack={addTrack} />
+        )}
+      </div>
+    </>
+  );
+}
+
+function EmptyTrackList() {
+  return (
+    <>
+      <div className="tracklist__empty">
+        <img src={trackListImage} alt="" />
+        <p>No tracks</p>
+      </div>
+    </>
+  );
+}
+
+function TrackListRender({ tracksData, addTrack }) {
+  return (
+    <>
+      {tracksData.map((track) => {
+        return (
+          <div className="tracklist__track" key={track.id} onClick={addTrack}>
+            {track.name}
+          </div>
+        );
+      })}
     </>
   );
 }
